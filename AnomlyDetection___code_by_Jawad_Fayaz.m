ad3e0f0ec28d6dcdd157a385078af6da670cf5e9
jfayaz <jfayaz@uci.edu>
2019-01-26 21:26:07 -0800

    added website

--- AnomlyDetection___code_by_Jawad_Fayaz.m
+++ AnomlyDetection___code_by_Jawad_Fayaz.m
@@ -1,6 +1,7 @@
 clear ; close all; clc; addpath('functions')
 %% ====================== ANOMALY DETECTION ============================ %%
 %  written by : JAWAD FAYAZ (email: jfayaz@uci.edu)
+%  visit: (https://jfayaz.github.io)
 
 %  ------------- Instructions -------------- %
 %  INPUT:
